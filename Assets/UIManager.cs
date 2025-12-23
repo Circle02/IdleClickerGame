45bec1d20e0240b1dc423941f8e65ac9b858f6ce
Bar Bitton <bar11mil@gmail.com>
2025-12-23 22:31:24 +0200

    added UpgradeButton, upgrade logic, UpgradeCostText UI, moved UIManager to a seperate game object

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -4,9 +4,13 @@ using TMPro;
 public class UIManager : MonoBehaviour
 {
     public TextMeshProUGUI coinText;
+    public TextMeshProUGUI upgradeCostText;
+    public UpgradeButton upgradeButton;
 
     void Update()
     {
-        coinText.text = $"Coins: {GameManager.Instance.coins:F1}";
+        coinText.text = $"Coins: {GameManager.Instance.coins:F0}";
+        if (upgradeCostText != null && upgradeButton != null)
+            upgradeCostText.text = $"Upgrade: {upgradeButton.upgradeCost:F0}";
     }
 }
---
+++ Assets/UpgradeButton.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class UpgradeButton : MonoBehaviour
+{
+    public double upgradeCost = 10;
+    public double cpsIncrease = 1;
+
+    public void BuyUpgrade()
+    {
+        if (GameManager.Instance.coins >= upgradeCost)
+        {
+            GameManager.Instance.coins -= upgradeCost;
+            GameManager.Instance.coinsPerSecond += cpsIncrease;
+            upgradeCost *= 1.2;
+        }
+    }
+}
